76f199b4e305b7471fff62370a66055df7835959
jample <smartljb@hotmail.com>
2024-12-04 20:52:08 +0800

    add new comments

--- src/test/java/org/springframework/samples/petclinic/system/CrashControllerIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/system/CrashControllerIntegrationTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+//add new comments!
 
 package org.springframework.samples.petclinic.system;
 
